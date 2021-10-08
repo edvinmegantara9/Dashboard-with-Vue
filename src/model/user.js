ba80bf4dc6fc9763d31f280279bdada834cded4c
aditrio <30noadi@gmail.com>
2021-10-08 13:03:29 +0700

    stable

--- src/model/user.js
+++ src/model/user.js
@@ -4,6 +4,6 @@ export const fields = [
     { key: 'nip', label: "NIP", _style:'min-width:100px' },
     { key: "group", label: "Golongan", _style:'min-width:50px'},
     { key: 'role', _style:'min-width:100px;' },
-    { key: 'position', label: "Position", _style:'min-width:100px;' },
+    { key: 'position', label: "Jabatan", _style:'min-width:100px;' },
     { key: "action", label: 'Aksi', _style: 'min-width:75px'}
 ]
