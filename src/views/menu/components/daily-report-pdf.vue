fb00ab2ec58d3a1cc8d7d5c469f650189460a4cf
werlic <agussulaeman453@yahoo.co.id>
2021-11-27 17:30:24 +0700

    add daily report pdf template

---
+++ src/views/menu/components/daily-report-pdf.vue
@@ -0,0 +1,79 @@
+<style>
+    #content-table table, #content-table th, #content-table td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+</style>
+<template>
+    <div>
+        <header>
+            <table style="width: 100%">
+                <tr>
+                    <td><img src="@/assets/logo/pagaralam.png" height="75px"></td>
+                    <td style="text-align: center"><h4><b>LAPORAN KINERJA HARIAN ASN BAPPEDA KOTA PAGAR ALAM</b></h4></td>
+                </tr>
+            </table>
+        </header>
+        <section>
+            <table id="content-table">
+                <thead style="text-align: center; border-">
+                    <th>Tanggal</th>
+                    <th>Jam</th>
+                    <th>Nama</th>
+                    <th>NIP</th>
+                    <th>Golongan</th>
+                    <th>Jabatan</th>
+                    <th>Laporan</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>23-12-2021</td>
+                        <td>00:00</td>
+                        <td>Tes</td>
+                        <td>11xxxxxxxx</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>Laporan....</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+        <br/>
+        <br/>
+        <footer>
+            <table>
+                <tr>
+                    <td style="width: 30%; text-align: center">
+                        Mengetahui<br/>
+                        KEPALA BAPPEDA KOTA PAGAR ALAM<br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <b>NOVI APRIYADI, SE,MM</b><br/>
+                        Pembina TK. I<br/>
+                        NIP. 197711052003121004
+                    </td>
+                    <td style="width: 30%"></td>
+                    <td style="width: 30%; text-align: center">
+                        Pagar Alam, ................................ 2021<br/>
+                        KEPALA SUB BAGIAN UMUM DAN KEPEGAWAIAN<br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <b>WARDALENA, SE</b><br/>
+                        PENATA TK. I<br/>
+                        NIP. 197611042007012013
+                    </td>
+                </tr>
+            </table>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
